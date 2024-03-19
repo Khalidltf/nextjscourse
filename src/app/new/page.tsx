02c494fa00de6e2bce87f62d8f7d7b33bea2a6cd
khalidlotfi <khalidlotfi2005@gmail.com>
2024-03-19 08:58:35 +0000

    home page - header

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,15 +1,14 @@
 import Link from 'next/link'
 
-export default function Home() {
+export default function New() {
   return (
     <>
       <header className='flex justify-between items-center mb-4'>
-        <h1 className='text-2xl' >Todos:</h1>
+        <h1 className='text-2xl' >New TODO: </h1>
         <Link
           className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
-          href='/new'>New</Link>
+          href='/'>home</Link>
       </header>
-      <ul></ul>
     </>
-  );
+  )
 }
